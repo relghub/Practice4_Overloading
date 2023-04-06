913c13c6ec3bee562aa4b7788192d2bc61f68aef
wideprism <wideprism@DESKTOP-JANCIF2>
2023-04-06 12:41:10 +0300

    Add project files.

---
+++ overloading/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Write("Введіть значення аргумента x: ");
+double x = double.Parse(Console.ReadLine());
+Console.Write("Введіть значення аргумента y: ");
+double y = double.Parse(Console.ReadLine());
+double f;
+if (x >= 2 && x < 5 && y < 3)
+{
+    f = EvalObj.Evaluation(ref x, ref y);
+}
+else if (y >= 3)
+{
+    f = EvalObj.Evaluation(ref y);
+}
+else
+{
+    f = EvalObj.Evaluation(x, y);
+}
+Console.WriteLine($"Розв'язком системи рівнянь є {f}");
+class EvalObj
+{
+    public static double Evaluation(ref double x, ref double y)
+    {
+        return x - y + Math.Pow(2, x);
+    }
+    public static double Evaluation(ref double y)
+    {
+        return 5 - 6 * y;
+    }
+    public static double Evaluation(double ArgumentX, double ArgumentY)
+    {
+        return 33.3 - 2 * Math.Pow(ArgumentX - ArgumentY, 2);
+    }
+}
